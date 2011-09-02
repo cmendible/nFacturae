f0b6b2fa7e19fdedff8cb17b834b0f114bf48e74
Carlos Mendible <cmendible@gmail.com>
2011-09-02 10:40:41 +0200

    Starting to add some helper methods.

---
+++ nFacturae/Fluent/FluentFacturae32.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nFacturae.Facturae32
+{
+    public partial class Facturae
+    {
+        public Facturae(string reference, DateTime issueDate)
+        { 
+
+        }
+
+        public Facturae AddSellerParty()
+        {
+            return this;
+        }
+
+        public Facturae AddBuyerParty()
+        {
+            return this;
+        }
+
+        public Facturae AddInvoiceLine()
+        {
+            return this;
+        }
+    }
+}
